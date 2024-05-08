e796fca43acd84782d96f2f6e6bb38357b6ce036
Abdul Rehman Ali <118052743+Abdul-Rehman-Ali@users.noreply.github.com>
2024-05-08 20:25:42 +0500

    c

--- connection.php
+++ connection.php
@@ -1,10 +1,14 @@
 <?php
-
 $server = "localhost";
 $username = "root";
 $password = "";
 $db_name = "smartloop";
 
+$con = mysqli_connect($server, $username, $password, $db_name);
 
-
+if ($con){
+    echo "Connected";
+} else {
+    echo "Not Connected";
+}
 ?>
