7e2cddc7d29340d5083e5b6d275922dfdc66b57f
Abdul Rehman Ali <118052743+Abdul-Rehman-Ali@users.noreply.github.com>
2024-05-08 21:10:12 +0500

    c

---
+++ terms.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Learning </title>
+    <link rel="stylesheet" href="Asserts/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="Asserts/css/font_awosome_icon.css">
+    <script src="Asserts/js/bootstrap.min.js"></script>
+</head>
+<script src="Asserts/js/font_awesome.js"> </script>
+
+<body>
+    <!-- Header -->
+    <?php
+    include("header.php");
+    ?>
+
+    <div class="container-fluid text-center py-5 privacy_header">
+      <h1><span>SmartLoop</span> Terms of Use</h1>
+    </div>
+    <div class="container terms_content">
+        <p>Welcome to SmartLoop! These Terms of Use ("Terms") govern your use of the SmartLoop website, located at www.smartloop.com, and any other websites or applications owned or operated by SmartLoop (collectively, the "Site"). By accessing or using the Site, you agree to be bound by these Terms. If you do not agree to these Terms, please do not use the Site.</p>
+     <ol>
+        <li>
+            <b>Acceptance of Terms: </b>
+            By using the Site, you agree to be bound by these Terms and any additional terms and conditions that are referenced herein or that otherwise may apply to specific areas of the Site, in each case as may be updated from time to time. SmartLoop reserves the right to change these Terms at any time, effective immediately upon posting on the Site. Your continued use of the Site constitutes acceptance of such changes.
+        </li>
+
+        <li>
+            <b>Use of the Site: </b>
+            SmartLoop provides a platform for online education courses. You may use the Site for lawful purposes only. You agree not to use the Site in any way that violates any applicable federal, state, local, or international law or regulation.
+        </li>
+
+        <li>
+            <b>Intellectual Property: </b>
+            The content, trademarks, service marks, and logos displayed on the Site ("SmartLoop Content") are owned by or licensed to SmartLoop and are subject to copyright and other intellectual property rights under the law. You may not use, reproduce, modify, distribute, or display any of the SmartLoop Content without the prior written consent of SmartLoop.
+        </li>
+
+        <li>
+            <b>User Accounts: </b>
+            In order to access certain features of the Site, you may be required to create a user account. You are responsible for maintaining the confidentiality of your account and password and for restricting access to your computer. You agree to accept responsibility for all activities that occur under your account or password.
+        </li>
+
+        <li>
+            <b>Privacy:</b>
+            SmartLoop respects the privacy of its users. Please review our <a href="privacy_policy.html">Privacy Policy</a>, which governs the collection and use of personal information on the Site.
+        </li>
+
+        <li>
+            <b>Third-Party Links: </b>
+            The Site may contain links to third-party websites or resources. SmartLoop is not responsible for the content, products, or services on or available through any such third-party websites or resources. You acknowledge and agree that SmartLoop shall not be liable for any loss or damage arising from your use of any third-party website or resource.
+        </li>
+
+        <li>
+            <b>Disclaimer of Warranties: </b>
+            The Site is provided on an "as is" and "as available" basis, without any warranties of any kind, either express or implied. SmartLoop disclaims all warranties, including, but not limited to, implied warranties of merchantability, fitness for a particular purpose, and non-infringement.
+        </li>
+
+        <li>
+            <b>Limitation of Liability: </b>
+            In no event shall SmartLoop be liable for any indirect, incidental, special, consequential, or punitive damages, including, without limitation, loss of profits, data, use, goodwill, or other intangible losses, arising out of or in connection with your use of the Site.
+        </li>
+
+        <li>
+            <b>Indemnification: </b>
+            You agree to indemnify, defend, and hold harmless SmartLoop, its affiliates, officers, directors, employees, agents, licensors, and suppliers from and against all claims, losses, liabilities, expenses, damages, and costs, including reasonable attorneys' fees, arising out of or in connection with your use of the Site or any violation of these Terms.
+        </li>
+
+        <li>
+            <b>Governing Law:  </b>
+            These Terms shall be governed by and construed in accordance with the laws of Pakistan Country, without regard to its conflicts of law principles.
+        </li>
+
+        <li>
+            <b>Contact Us:   </b>
+            If you have any questions about these Terms, please <a href="contactUs.html">contact us</a> at contact@smartloop.com.
+        </li>
+     </ol>
+
+     <p>By using the Site, you acknowledge that you have read, understood, and agree to be bound by these Terms. If you do not agree to these Terms, you may not access or use the Site.</p>
+    </div>
+
+    <!-- footer -->
+    <?php
+    include("footer.php");
+    ?>
+
+  </body>
+</html>
+
+
