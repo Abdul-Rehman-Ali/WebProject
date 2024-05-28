378fa0447af760a088899e79fa071861eecac4b2
Abdul Rehman Ali <118052743+Abdul-Rehman-Ali@users.noreply.github.com>
2024-05-28 10:14:07 +0500

    c

--- aboutUs.php
+++ aboutUs.php
@@ -69,63 +69,9 @@
 
         <!-- Certified teachers heading-->
    
-     <div class="home_testimonial_heading text-center">
-        <p class="home_testimonial_p">
-          OUR STAFF
-        </p>
-  
-        <div>
-          <h3>Certified Teachers</h3>
-        </div>
-  
-        <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
-      </div> 
-
-        <!-- Certified teachers-->
-
-
-    <div class="container-fluid px-6 certified_teacher text-center py-3px">
-        <div class="row">
-          
-          <div class=" col-md-6 col-lg-3 mb-4">
-            <img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="...">
-            <div class="card-body certified_teacher_name">
-              <h3>Bianca Wilson</h3>
-              <span>Teacher</span>
-              <p class="card-text">I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-            </div>
-          </div>
-  
-          <div class=" col-md-6 col-lg-3 mb-4">
-            <img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="...">
-            <div class="card-body certified_teacher_name">
-              <h3>Bianca Wilson</h3>
-              <span>Teacher</span>
-              <p class="card-text">I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-            </div>
-          </div>
-  
-          <div class=" col-md-6 col-lg-3 mb-4">
-            <img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="...">
-            <div class="card-body certified_teacher_name">
-              <h3>Bianca Wilson</h3>
-              <span>Teacher</span>
-              <p class="card-text">I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-            </div>
-          </div>
-  
-          <div class=" col-md-6 col-lg-3 mb-4">
-            <img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="...">
-            <div class="card-body certified_teacher_name">
-              <h3>Bianca Wilson</h3>
-              <span>Teacher</span>
-              <p class="card-text">I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-            </div>
-          </div>
-  
-        </div>
-  
-      </div>
+        <?php
+          include("our_staff.php");
+        ?>
 
 
       <!-- space -->
