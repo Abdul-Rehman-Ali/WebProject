c9db2e3b4a2fb8d6ecc4809d8d29ef6a9dce31b5
Abdul Rehman Ali <118052743+Abdul-Rehman-Ali@users.noreply.github.com>
2024-05-28 10:10:09 +0500

    c

---
+++ our_courses.php
@@ -0,0 +1,98 @@
+<div class="home_course text-center">
+<p class="home_course_p">
+  OUR COURSES
+</p>
+
+<div>
+  <h3>Explore Our Popular Online Courses</h3>
+</div>
+
+<p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
+</div>
+
+<!-- cards Courses -->
+<div class="container-fluide home_courses">
+<div class="row home_offer_courses">
+
+<div class="card home_courses col-lg-3 col-md-6" style="width: 18rem;">
+<img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
+<div class="card-body">
+  <h5 class="card-title">Web Development</h5>
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <a href="web_development.html" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
+</div>
+</div>
+
+<div class="card home_courses col-lg-3 col-md-6" style="width: 18rem;">
+<img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
+<div class="card-body">
+  <h5 class="card-title">Shopify</h5>
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <a href="#" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
+</div>
+</div>
+
+
+<div class="card home_courses col-lg-3 col-md-6" style="width: 18rem;">
+<img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
+<div class="card-body">
+  <h5 class="card-title">Web Development</h5>
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <a href="#" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
+</div>
+</div>
+
+<div class="card home_courses col-lg-3 col-md-6" style="width: 18rem;">
+<img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
+<div class="card-body">
+  <h5 class="card-title">App Development</h5>
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <a href="#" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
+</div>
+</div>
+</div>
+</div>
+
+<!-- 2nd Row of Courses -->
+
+<div class="container-fluide home_courses">
+<div class="row home_offer_courses">
+
+<div class="card home_courses col-lg-3 col-md-6" style="width: 18rem;">
+  <img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
+  <div class="card-body">
+    <h5 class="card-title">SEO Course</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
+  </div>
+</div>
+
+<div class="card home_courses col-lg-3 col-md-6" style="width: 18rem;">
+  <img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
+  <div class="card-body">
+    <h5 class="card-title">Python Expert</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
+  </div>
+</div>
+
+
+<div class="card home_courses col-lg-3 col-md-6" style="width: 18rem;">
+  <img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
+  <div class="card-body">
+    <h5 class="card-title">Artificial Intelligence</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
+  </div>
+</div>
+
+<div class="card home_courses" style="width: 18rem;">
+  <img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
+  <div class="card-body">
+    <h5 class="card-title">Digital Marketing</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
+  </div>
+</div>
+</div>
+</div>
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+
+echo 'PHP version: ' . phpversion();
+
+?>
