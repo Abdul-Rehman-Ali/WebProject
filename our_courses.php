6da66b7475be45cef3c8ba3c6cfc06ba536bf898
Abdul Rehman Ali <118052743+Abdul-Rehman-Ali@users.noreply.github.com>
2024-05-28 11:24:23 +0500

    c

--- our_courses.php
+++ our_courses.php
@@ -1,98 +1,56 @@
-<div class="home_course text-center">
-<p class="home_course_p">
-  OUR COURSES
-</p>
+<?php
+    // Include the database connection file
+    require('connection.php');
 
-<div>
-  <h3>Explore Our Popular Online Courses</h3>
-</div>
+    // Query to fetch data from the 'courses' table
+    $sql = "SELECT * FROM courses LIMIT 8"; // Fetch only 8 records
+    $result = $con->query($sql);
+?>
 
-<p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
-</div>
+<!-- Centered container with equal spacing on both sides -->
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="home_course text-center">
+                <p class="home_course_p">
+                    OUR COURSES
+                </p>
 
-<!-- cards Courses -->
-<div class="container-fluide home_courses">
-<div class="row home_offer_courses">
+                <div>
+                    <h3>Explore Our Popular Online Courses</h3>
+                </div>
 
-<div class="card home_courses col-lg-3 col-md-6" style="width: 18rem;">
-<img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
-<div class="card-body">
-  <h5 class="card-title">Web Development</h5>
-  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  <a href="web_development.html" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
-</div>
-</div>
+                <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
+            </div>
+        </div>
+    </div>
 
-<div class="card home_courses col-lg-3 col-md-6" style="width: 18rem;">
-<img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
-<div class="card-body">
-  <h5 class="card-title">Shopify</h5>
-  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  <a href="#" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
-</div>
-</div>
-
-
-<div class="card home_courses col-lg-3 col-md-6" style="width: 18rem;">
-<img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
-<div class="card-body">
-  <h5 class="card-title">Web Development</h5>
-  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  <a href="#" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
-</div>
-</div>
-
-<div class="card home_courses col-lg-3 col-md-6" style="width: 18rem;">
-<img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
-<div class="card-body">
-  <h5 class="card-title">App Development</h5>
-  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  <a href="#" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
-</div>
-</div>
-</div>
-</div>
-
-<!-- 2nd Row of Courses -->
-
-<div class="container-fluide home_courses">
-<div class="row home_offer_courses">
-
-<div class="card home_courses col-lg-3 col-md-6" style="width: 18rem;">
-  <img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
-  <div class="card-body">
-    <h5 class="card-title">SEO Course</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
-  </div>
-</div>
-
-<div class="card home_courses col-lg-3 col-md-6" style="width: 18rem;">
-  <img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
-  <div class="card-body">
-    <h5 class="card-title">Python Expert</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
-  </div>
-</div>
-
-
-<div class="card home_courses col-lg-3 col-md-6" style="width: 18rem;">
-  <img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
-  <div class="card-body">
-    <h5 class="card-title">Artificial Intelligence</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
-  </div>
-</div>
-
-<div class="card home_courses" style="width: 18rem;">
-  <img src="Asserts/images/homeBanner.webp" class="card-img-top" alt="Our Courses">
-  <div class="card-body">
-    <h5 class="card-title">Digital Marketing</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>
-  </div>
-</div>
-</div>
+    <!-- cards Courses -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="container-fluide home_courses">
+                <div class="row home_offer_courses">
+                    <?php
+                        // Check if there are any rows returned from the query
+                        if ($result->num_rows > 0) {
+                            // Loop through each row of data
+                            while($row = $result->fetch_assoc()) {
+                                echo '<div class="card home_courses col-lg-3 col-md-6" style="width: 18rem;">';
+                                echo '<img src="' . $row["image"] . '" class="card-img-top" alt="Our Courses">';
+                                echo '<div class="card-body">';
+                                echo '<h5 class="card-title">' . $row["title"] . '</h5>';
+                                echo '<p class="card-text">' . $row["description"] . '</p>';
+                                echo '<a href="' . $row["link"] . '" class="btn btn-primary home_course_enroll_btn">Enroll Now</a>';
+                                echo '</div>';
+                                echo '</div>';
+                            }
+                        } else {
+                            // If no rows are returned, display a message
+                            echo "No courses available.";
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
